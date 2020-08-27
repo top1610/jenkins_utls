a37f5d29f1257a6b0fb6d59ff874dc37f9cba2c8
top1610 <57243712+top1610@users.noreply.github.com>
2020-08-27 11:05:18 +0700

    Update pythonPipeline.groovy

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -27,9 +27,9 @@ def call(String gitUrl, String helmConfig, String appType, String projectName, S
                     )
                     sh "cp ${WORKSPACE}/${configDir}/${REGION}/config_${ENV}_env.py ${WORKSPACE}/${libDir}/config.py"
                     sh "cp ${WORKSPACE}/deploy_config/Dockerfile ${WORKSPACE}/Dockerfile"
-                    sh "sed -i '\"s+\\[HOST_PORT\\]+${HOST_PORT}+g\" $ZIP_PROJECT/common/deploy/*'
-                    sh "sed -i '\"s+\\[NUM_WORKERS\\]+${NUM_WORKERS}+g\" $ZIP_PROJECT/common/deploy/*'
-                    sh "sed -i '\"s+\\[DOCKER_SERVICE\\]+${DOCKER_SERVICE}+g" $ZIP_PROJECT/common/deploy/*'
+                    sh "sed -i '\"s+\\[HOST_PORT\\]+${HOST_PORT}+g\" common/deploy/*'
+                    sh "sed -i '\"s+\\[NUM_WORKERS\\]+${NUM_WORKERS}+g\" common/deploy/*'
+                    sh "sed -i '\"s+\\[DOCKER_SERVICE\\]+${DOCKER_SERVICE}+g\" common/deploy/*'
                     script {
                         def appimage = docker.build (dockerImage + ":$BUILD_NUMBER", "--network=host .")
                         docker.withRegistry( 'https://registry.cooky.vn', 'hieupham-cooky-git' ) {
