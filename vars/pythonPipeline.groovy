e4e82451a590e976d24530b7b2b2e6b858dab66d
top1610 <57243712+top1610@users.noreply.github.com>
2020-08-27 15:25:28 +0700

    Update pythonPipeline.groovy

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -33,7 +33,6 @@ def call(String gitUrl, String helmConfig, String appType, String projectName, S
                     sh "cp ${WORKSPACE}/deploy_config/Dockerfile ${WORKSPACE}/Dockerfile"
                     sh "sed -i \"s+\\[HOST_PORT\\]+${HOST_PORT}+g\" common/deploy/*"
                     sh "sed -i \"s+\\[NUM_WORKERS\\]+${NUM_WORKERS}+g\" common/deploy/*"
-                    sh "sed -i \"s+\\[DOCKER_SERVICE\\]+${DOCKER_SERVICE}+g\" common/deploy/*"
                     script {
                         def appimage = docker.build (dockerImage + ":$BUILD_NUMBER", "--network=host .")
                         docker.withRegistry( 'https://registry.cooky.vn', 'hieupham-cooky-git' ) {
