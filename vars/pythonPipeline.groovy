ee155bbd9add354406b74514ac749170eb975a93
top1610 <57243712+top1610@users.noreply.github.com>
2020-08-28 13:43:38 +0700

    Update pythonPipeline.groovy

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -30,7 +30,7 @@ def call(String gitUrl, String helmConfig, String appType, String projectName, S
                     
                     sh "cp ${WORKSPACE}/${configDir}/${REGION}/config_${ENV}_env.py ${WORKSPACE}/${libDir}/config.py"
                     sh "cp ${WORKSPACE}/deploy_config/Dockerfile ${WORKSPACE}/Dockerfile"
-                    sh "cp ${WORKSPACE}/common/gunicorn_config.py ${WORKSPACE}/gunicorn_config.py"
+                    //sh "cp ${WORKSPACE}/common/gunicorn_config.py ${WORKSPACE}/gunicorn_config.py"
                     //sh "sed -i \"s+\\[HOST_PORT\\]+${HOST_PORT}+g\" common/deploy/*"
                     //sh "sed -i \"s+\\[NUM_WORKERS\\]+${NUM_WORKERS}+g\" common/deploy/*"
                     script {
